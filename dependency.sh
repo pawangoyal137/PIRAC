39c71117759a7ac24b0dd44a741407eff1de31ab
Pawan Goyal <pawangoyal2000@gmail.com>
2023-04-04 00:40:39 -0400

    added scripts for setup

---
+++ dependency.sh
@@ -0,0 +1,5 @@
+sudo apt-get update 
+sudo apt-get install golang-go
+sudo apt-get install build-essential
+sudo apt install libssl-dev
+sudo apt-get install cmake
---
+++ python_packages.sh
@@ -0,0 +1,3 @@
+sudo apt install python3-pip
+sudo apt-get install python3-numpy
+sudo apt-get install python3-matplotlib
