2e55b1354d509e891b049d49ba5ec475a52017e9
Pawan Goyal <pawangoyal2000@gmail.com>
2023-04-05 01:56:58 -0400

    added benchmark for the pallier together with the readme

---
+++ paillier/README.md
@@ -0,0 +1,8 @@
+* Run following command to run all benchmarks
+```
+go test -bench=Benchmark
+```
+* Run following command to run a particular benchmark 
+```
+go test -bench=<Name of the benchmark>
+```
--- paillier/paillier_test.go
+++ paillier/paillier_test.go
@@ -75,3 +75,37 @@ func BenchmarkExponentModDifPrimes(b *testing.B) {
     averageTime := totalTime / time.Duration(numIter)
     fmt.Printf("Average time for exponentiation per iteration over %d loops: %v\n", numIter, averageTime)
 }
+
+func BenchmarkPaillier(b *testing.B) {
+    bits := 3072
+    x := generateRandomNumber(bits)
+    y := generateRandomNumber(bits)
+    z := generateCompositeNumber(bits)
+
+	b.ResetTimer()
+    for n := 0; n < b.N; n++ {
+        new(big.Int).Exp(x, y, z)
+        new(big.Int).Mod(new(big.Int).Mul(x, y), z)
+    }
+}
+
+func BenchmarkPaillierDifPrimes(b *testing.B) {
+    bits := 3072
+
+	numIter := int(math.Max(float64(NUMITERS), float64(b.N)))
+	var totalTime time.Duration
+    for i := 0; i < numIter; i++ {
+		x := generateRandomNumber(bits)
+		y := generateRandomNumber(bits)
+		z := generateCompositeNumber(bits)
+
+        start := time.Now()
+        new(big.Int).Exp(x, y, z)
+        new(big.Int).Mod(new(big.Int).Mul(x, y), z)
+        totalTime += time.Since(start)
+    }
+
+    // calculate and print the average time taken per iteration
+    averageTime := totalTime / time.Duration(numIter)
+    fmt.Printf("Average time for paillier per iteration over %d loops: %v\n", numIter, averageTime)
+}
