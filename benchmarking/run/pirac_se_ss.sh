0d8e015c72b8395f931a55d0fae4182047cf15a7
Pawan Goyal <pawangoyal2000@gmail.com>
2023-04-26 04:14:20 +0000

    new results

--- benchmarking/run/pirac_se_ss.sh
+++ benchmarking/run/pirac_se_ss.sh
@@ -1,15 +1,15 @@
-OUTPUT_FOLDER=results/output/
+OUTPUT_FOLDER=results/output
 JSON_FILE_FOLDER=results/data
-EVAL_FILE=bench-single-server/pir.py
+EVAL_FILE=pir/pir.py
 
 pir_schemes=("simplepir" "spiralpir" "spiralstream" "spiralpack" "spiralstreampack" "sealpir" "fastpir")
 
 for i in "${!pir_schemes[@]}"; do 
-    OUTPUT_FILE = "$OUTPUT_FOLDER/${pir_schemes[i]}.txt"
+    OUTPUT_FILE="${OUTPUT_FOLDER}/${pir_schemes[i]}.txt"
     if test -f "$OUTPUT_FILE"; then
         truncate -s 0 "$OUTPUT_FILE"
     fi
-    python3 "$EVAL_FILE" -n "${pir_schemes[i]}" -w "$JSON_FILE_FOLDER/${pir_schemes[i]}.json" -pm bl mp fs &>> "$OUTPUT_FILE"
+    python3 "$EVAL_FILE" -n "${pir_schemes[i]}" -ds 16 18 20 -ni 5 -w "$JSON_FILE_FOLDER/${pir_schemes[i]}.json" -pm bl mp fs &>> "$OUTPUT_FILE"
     echo "-----------------------------------------------"
     echo "Final Results are as follows:"
     grep -E "^Range of|^Overhead of" "$OUTPUT_FILE" 
