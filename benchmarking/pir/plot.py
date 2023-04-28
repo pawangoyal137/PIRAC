bfab0404e6840bd64553b737cf92a937d03b4921
Pawan Goyal <pawangoyal2000@gmail.com>
2023-04-27 20:37:22 -0400

    corrected the plot code

--- benchmarking/pir/plot.py
+++ benchmarking/pir/plot.py
@@ -44,6 +44,20 @@ parser.add_argument('-f','--figName',
                      required=False, type=str,
                      help='Name of the saved image')
 
+def generate_results(log2_db_size, elem_sizes, tput_columns, verbose=False):
+    df_all = utils.concatenate_jsons("results/data", verbose)
+    
+    # create new dataframe with only those rows corresponding to elem_sizes
+    df_es = df_all[(df_all['log2_db_size'] == log2_db_size)
+                            & (df_all['elem_size'].isin(elem_sizes))]
+    assert len(df_es)==len(elem_sizes)
+
+    col_filter = ["elem_size"]+tput_columns
+    df_col = df_es[col_filter]
+
+    if verbose:
+        print(df_col)
+    return df_col
 
 def gen_elem_comp_plot(fig_name):
     """
@@ -53,32 +67,25 @@ def gen_elem_comp_plot(fig_name):
     PIR_LABELS = ["SpiralPIR", "SpiralStreamPack", "SealPIR", "FastPIR"]
     PIRAC_MODES = ["bl", "mp", "fs"]
 
-    def generate_results(log2_db_size, elem_sizes, verbose=False):
-        df_all = utils.concatenate_jsons("results/data", verbose)
-        
-        # create new dataframe with only those rows corresponding to elem_sizes
-        df_es = df_all[(df_all['log2_db_size'] == log2_db_size)
-                                & (df_all['elem_size'].isin(elem_sizes))]
-        assert len(df_es)==len(elem_sizes)
-
-        tput_columns = [f"{pir_name}_{pirac_mode}_tput" for pir_name in PIR_NAMES
-                                                        for pirac_mode in PIRAC_MODES]
-        col_filter = ["elem_size"]+tput_columns
-        df_col = df_es[col_filter]
-        assert len(PIR_NAMES)*len(PIRAC_MODES)==sum(['tput' in col for col in df_col.columns])
-
-        if verbose:
-            print(df_col)
-        return df_col
-
     # get results
-    results = generate_results(utils.LOG2_DB_SIZE, utils.ELEM_SIZES)
+    tput_columns = [f"{pir_name}_{pirac_mode}_tput" for pir_name in PIR_NAMES
+                                                    for pirac_mode in PIRAC_MODES]
+    results = generate_results(utils.LOG2_DB_SIZE, utils.ELEM_SIZES, tput_columns)
     x_values = results["elem_size"].values
 
-    # set image scale
-    plt.figure()
-    plt.yscale("log")
-    plt.xscale("log")
+    # create a 2 by 1 figure
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=False)
+
+    # set fig height 
+    fig.set_figheight(6)
+
+    # set figure scale
+    ax1.set_yscale("log")
+    ax1.set_xscale("log")
+    ax2.set_yscale("log")
+    ax2.set_xscale("log")
+    # plt.grid(True, linewidth=1, color='grey', alpha=0.5)
+
 
     colors = ['#08519c', '#ff7f00', '#16a085', '#8e44ad'] #, '#c0392b', '#333']
     cc = itertools.cycle(colors)
@@ -86,33 +93,44 @@ def gen_elem_comp_plot(fig_name):
 
     for pir in PIR_NAMES:
         c = next(cc)
-        l1, = plt.plot(x_values, results[f'{pir}_bl_tput'].values, color=c, linestyle='-',  alpha=0.7)
-        l2, = plt.plot(x_values, results[f'{pir}_mp_tput'].values, color=c, linestyle='--')
-        l3, = plt.plot(x_values, results[f'{pir}_fs_tput'].values, color=c, linestyle=':')
-        plot_lines.append([l1, l2, l3])
+        l1, = ax1.plot(x_values, results[f'{pir}_bl_tput'].values, linewidth=2, color=c, linestyle='-', label=pir) #, marker='o', markersize=4)
+        l2, = ax1.plot(x_values, results[f'{pir}_mp_tput'].values, linewidth=2, color=c, linestyle='--')
+        l3, = ax2.plot(x_values, results[f'{pir}_bl_tput'].values, linewidth=2, color=c, linestyle='-') #, marker='o', markersize=4)
+        l4, = ax2.plot(x_values, results[f'{pir}_fs_tput'].values, linewidth=2, color=c, linestyle=':')
+        plot_lines.append([l1, l2, l3, l4])
     
+
+    # set labels, x axis is common
+    ax1.set_ylabel("Throughput (in MB/s)")
+    ax2.set_xlabel("Entry size (in bytes)")
+    ax2.set_ylabel("Throughput (in MB/s)")
+
+    # use tight layout to avoid any empty side spaces
+    fig.tight_layout()
+
+    #create space for legend at the bottom and add the legend
+    ax1.legend(plot_lines[0][:2], ["Baseline (& Pirac w/ basic auth)", "Pirac w/ metadata-private auth"],
+                          loc="lower right", framealpha=1)
+    ax2.legend(plot_lines[0][2:], ["Baseline (& Pirac w/ basic auth)", "Pirac w/ forward-secret auth"],
+                          loc="lower right", framealpha=1)
+    plt.subplots_adjust(bottom=0.14)
+    handles, _ = ax1.get_legend_handles_labels()
+    fig.legend(handles, PIR_LABELS, loc="lower left", ncol=len(PIR_LABELS), 
+                                    bbox_to_anchor=(0.1, 0), framealpha=1)
     
-    legend1 = plt.legend([l[0] for l in plot_lines], PIR_LABELS, loc="upper left")
-    legend2 = plt.legend(plot_lines[0], ["Baseline", "With MP", "With FS"], loc="lower right")
-    plt.gca().add_artist(legend1)
-    plt.gca().add_artist(legend2)
-
-    plt.xlabel("Entry size (in bytes)")
-    plt.ylabel("Throughput (in MB/s)")
-
-    plt.tight_layout()
-    plt.savefig(f"images/{fig_name}.pdf", dpi=300)
+    # save the figure
+    fig.savefig(f"images/{fig_name}.pdf", dpi=300)
     print(f"Successfully generated and save the plot with name {fig_name}")
 
 def gen_batched_bar_plot(pir_name, batch_values, fig_name):
-    plt.figure().set_figheight(3)
-
+    PIR_NAME = "spiralstreampack"
+    tput_columns = [f"{PIR_NAME}_{pm}_tput" for pm in ["bl", "mp", "fs"]]
+    results = generate_results(utils.LOG2_DB_SIZE, [1<<15], tput_columns, verbose=False)
+    
     # calculate tputs
-    log_db_size = utils.LOG2_DB_SIZE
-    elem_size = (1<<15)
-    pir_tput = cal_pir_tput(pir_name, log_db_size, elem_size, {}, False, num_iter=5)
-    re_tput = cal_pirac_tput(log_db_size, elem_size,  5, rekeying = False)
-    pirac_tput = cal_pirac_tput(log_db_size, elem_size,  5, rekeying = True)
+    pir_tput = results[f'{PIR_NAME}_bl_tput'].values[0]
+    pir_mp_tput = results[f'{PIR_NAME}_mp_tput'].values[0]
+    pir_fs_tput = results[f'{PIR_NAME}_fs_tput'].values[0]
     
     # calculate batch tputs
     pir_tputs = []
@@ -120,8 +138,8 @@ def gen_batched_bar_plot(pir_name, batch_values, fig_name):
     pirac_tputs = []
     for bv in batch_values:
         pir_tputs.append(pir_tput)
-        re_tputs.append(utils.cal_tput_with_pirac(pir_tput, re_tput, bv))
-        pirac_tputs.append(utils.cal_tput_with_pirac(pir_tput, pirac_tput, bv))
+        re_tputs.append(utils.cal_tput_for_comb(pir_tput, pir_mp_tput, bv))
+        pirac_tputs.append(utils.cal_tput_for_comb(pir_tput, pir_fs_tput, bv))
     # set the width of each bar and colors
     barWidth = 0.25
     colors = ['#08519c', '#ff7f00', '#16a085', '#8e44ad', '#c0392b', '#333']
@@ -131,10 +149,16 @@ def gen_batched_bar_plot(pir_name, batch_values, fig_name):
     re_positions = [x + barWidth for x in pir_positions]
     pirac_positions = [x + barWidth for x in re_positions]
     
+    # set figure height
+    plt.figure().set_figheight(3)
+
     # create the bar graph
-    plt.bar(pir_positions, pir_tputs, color=colors[0], width=barWidth, edgecolor='white', label="Baseline",  hatch='////')
-    plt.bar(re_positions, re_tputs, color=colors[1], width=barWidth, edgecolor='white', label="With MP",  hatch='////')
-    plt.bar(pirac_positions, pirac_tputs, color=colors[2], width=barWidth, edgecolor='white', label="With FS",  hatch='////')
+    plt.bar(pir_positions, pir_tputs, color=colors[0], width=barWidth,
+         edgecolor='white', label="Baseline (& Pirac w/ basic auth)",  hatch='////')
+    plt.bar(re_positions, re_tputs, color=colors[1], width=barWidth,
+         edgecolor='white', label="Pirac w/ metadata-private auth",  hatch='////')
+    plt.bar(pirac_positions, pirac_tputs, color=colors[2], width=barWidth,
+         edgecolor='white', label="Pirac w/ forward-secret auth",  hatch='////')
     
     # add xticks on the middle of the group bars
     plt.xticks([i + barWidth for i in range(len(batch_values))], batch_values)
@@ -142,8 +166,8 @@ def gen_batched_bar_plot(pir_name, batch_values, fig_name):
     # add a legend
     plt.legend(loc="lower right") # bbox_to_anchor=(0.1, 1.0)
 
-    plt.xlabel("Number of batched entries (T)")
-    plt.ylabel("Throughput (MB/s)")
+    plt.xlabel("Number of queries between re-keying or re-encryption (T)")
+    plt.ylabel("Throughput (MB/s) \n (amortized over T queries)")
 
     # save the plot
     plt.tight_layout()
