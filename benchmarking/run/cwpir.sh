8b3fb2a0e1bb5719802d63e8aad2f07196ca18db
Pawan Goyal <pawangoyal2000@gmail.com>
2023-04-28 00:19:41 +0000

    new results for cwpir

--- benchmarking/run/cwpir.sh
+++ benchmarking/run/cwpir.sh
@@ -1,21 +1,25 @@
-OUTPUT_FILE=results/output/cwpir.txt
+OUTPUT_FOLDER=results/output/
 JSON_FILE_FOLDER=results/data
 EVAL_FILE=pir/pir.py
 
+# 48 bit keywords 
+OUTPUT_FILE="${OUTPUT_FOLDER}/cwpir_48.txt"
 if test -f "$OUTPUT_FILE"; then
     truncate -s 0 "$OUTPUT_FILE"
 fi
-
-# Baseline
-python3 "$EVAL_FILE" -n cwpir -w "$JSON_FILE_FOLDER/cwpir_baseline.json" -ds 10 -es 163840 327680 655360 1310720 2621440 -arg='{"h":2}' -o &>> "$OUTPUT_FILE"
-
-# 48 bit keywords
-# python3 "$EVAL_FILE" -n cwpir -w "$JSON_FILE_FOLDER/cwpir_48.json" -ds 10 -es 163840 327680 655360 1310720 2621440 -arg='{"kw:48", "h=5"}' -o &>> "$OUTPUT_FILE"
-
-# for ((h=2; h<=8; h++)); do
-#     python3 "$EVAL_FILE"  -ds 10 -es 163840 -hs $h -kw 48 -o &>> "$OUTPUT_FILE"
-# done
-
+python3 "$EVAL_FILE" -n cwpir -w "${JSON_FILE_FOLDER}/cwpir_48.json" -ds 10 -arg='{"kw":48, "h":5}' -o  &>> "$OUTPUT_FILE"
 echo "-----------------------------------------------"
 echo "Final Results are as follows:"
 grep -E "^Range of|^Overhead of" "$OUTPUT_FILE" 
+
+# Baseline
+OUTPUT_FILE="${OUTPUT_FOLDER}/cwpir_baseline.txt"
+if test -f "$OUTPUT_FILE"; then
+    truncate -s 0 "$OUTPUT_FILE"
+fi
+python3 "$EVAL_FILE" -n cwpir -w "$JSON_FILE_FOLDER/cwpir_baseline.json" -ds 10 -arg='{"h":2}' -o &>> "$OUTPUT_FILE"
+echo "-----------------------------------------------"
+echo "Final Results are as follows:"
+grep -E "^Range of|^Overhead of" "$OUTPUT_FILE" 
+
+# 128 512 2048 8192 32768
