5930e6518d3288775bb2583ce34472d3205126eb
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2023-05-04 10:58:43 -0400

    Make benchmarks clearer

--- pke/cmd/main.go
+++ pke/cmd/main.go
@@ -13,6 +13,11 @@ import (
 var NUM_TRIALS = 1000
 
 func main() {
+	benchmarkElGamalReRandomization()
+	benchmarkPaillierReRandomization()
+}
+
+func benchmarkElGamalReRandomization() {
 	pk, _ := elgamal.KeyGen(elliptic.P256(), 100)
 
 	db := make([]*elgamal.Ciphertext, NUM_TRIALS)
@@ -29,10 +34,17 @@ func main() {
 
 	totalTimeMS := time.Since(start).Milliseconds()
 	totalTimeS := float64(totalTimeMS) / 1000.0
-	fmt.Println(totalTimeS)
 
 	timePerCiphertextS := float64(totalTimeS) / float64(NUM_TRIALS)
+
 	// 32 bytes in MB = float64(32/(1000000))
 	throughputMBps := float64(32.0/(1000000)) / timePerCiphertextS
-	fmt.Printf("Throughput (MB/s) = %v\n", throughputMBps)
+
+	fmt.Printf("ElGamal re-randomization throughput (MB/s) = %v\n", throughputMBps)
+}
+
+func benchmarkPaillierReRandomization() {
+	// TODO: make the paillier code compatible
+	fmt.Printf("Paillier re-randomization throughput (MB/s) = TODO\n")
+
 }
