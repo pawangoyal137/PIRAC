414413deb54c524bf5f235de6c943630a8e9f04c
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2023-05-04 10:50:28 -0400

    Formatting and cleanup

--- acpir/include/encryption.h
+++ acpir/include/encryption.h
@@ -2,10 +2,6 @@
 #include <string.h>
 #include <stdint.h>
 
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
-
 typedef unsigned __int128 uint128_t;
 
 uint128_t *encryptDatabase(uint128_t *database, uint128_t seed, uint64_t size, uint64_t elemsize);
